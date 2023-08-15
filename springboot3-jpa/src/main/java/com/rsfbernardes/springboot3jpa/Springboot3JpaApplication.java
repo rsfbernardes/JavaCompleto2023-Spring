e7396a8800023a7bb83b133c04bbea176ae8a4aa
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-08-15 16:34:54 -0300

    Project created

---
+++ springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/Springboot3JpaApplication.java
@@ -0,0 +1,13 @@
+package com.rsfbernardes.springboot3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3JpaApplication.class, args);
+	}
+
+}
---
+++ springboot3-jpa/src/test/java/com/rsfbernardes/springboot3jpa/Springboot3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsfbernardes.springboot3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
