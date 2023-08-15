a00006e3a96e3e559ca4f83e3e98a6b7d0acb851
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-08-15 17:39:24 -0300

    H2 database, test profile, JPA, JPA repository, dependency injection

---
+++ springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rsfbernardes.springboot3jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rsfbernardes.springboot3jpa.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+}
--- springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/resources/UserResource.java
+++ springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/resources/UserResource.java
@@ -1,19 +1,26 @@
 package com.rsfbernardes.springboot3jpa.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rsfbernardes.springboot3jpa.entities.User;
+import com.rsfbernardes.springboot3jpa.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/users")
-public class UserResource {		
-
+public class UserResource {
+	
+	@Autowired
+	UserRepository userRepository;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Maria", "maria@gmail.com", "999991234", "12345");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> user = userRepository.findAll();
 		return ResponseEntity.ok().body(user);
 	}
 }
