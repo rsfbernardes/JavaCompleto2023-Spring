8f7fd5b68ecc55c5ad9f4fc50686ab864371fbd0
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-08-17 15:40:15 -0300

    Subtotal & Total methods

--- springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/entities/OrderItem.java
+++ springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return quantity * price;
+	}
 
 	@Override
 	public int hashCode() {
@@ -80,5 +84,5 @@ public class OrderItem implements Serializable {
 			return false;
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
-	}	
+	}
 }
