c4fb1665c3885052cccd7d564185fca573be871d
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-08-19 19:53:51 -0300

    User create

--- springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/resources/UserResource.java
+++ springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.rsfbernardes.springboot3jpa.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rsfbernardes.springboot3jpa.entities.User;
 import com.rsfbernardes.springboot3jpa.services.UserService;
@@ -19,6 +23,16 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 	
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody User user){
+		User newUser = userService.create(user);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequest().
+				path("/{id}").
+				buildAndExpand(newUser.getId()).toUri();
+		return ResponseEntity.created(uri).body(newUser);
+	}
+
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		List<User> userList = userService.findAll();
