250dc01d69ce27a8689c23d38bd19b46832a5494
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-08-19 21:13:39 -0300

    Exception handling - User delete

--- springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/services/UserService.java
+++ springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rsfbernardes.springboot3jpa.entities.User;
 import com.rsfbernardes.springboot3jpa.repositories.UserRepository;
+import com.rsfbernardes.springboot3jpa.services.exceptions.DatabaseException;
 import com.rsfbernardes.springboot3jpa.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User user) {
---
+++ springboot3-jpa/src/main/java/com/rsfbernardes/springboot3jpa/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.rsfbernardes.springboot3jpa.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String message) {
+		super(message);
+	}
+}
