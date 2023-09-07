14805ea2226b7af90edbfa2a75072f4c28e06759
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-09-07 18:13:56 -0300

    Projeção dos dados do autor com DTO

--- workshopmongo/src/main/java/com/rsfbernardes/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/rsfbernardes/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.rsfbernardes.workshopmongo.domain.Post;
 import com.rsfbernardes.workshopmongo.domain.User;
+import com.rsfbernardes.workshopmongo.dto.AuthorDto;
 import com.rsfbernardes.workshopmongo.repositories.PostRepository;
 import com.rsfbernardes.workshopmongo.repositories.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDto(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
---
+++ workshopmongo/src/main/java/com/rsfbernardes/workshopmongo/dto/AuthorDto.java
@@ -0,0 +1,36 @@
+package com.rsfbernardes.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.rsfbernardes.workshopmongo.domain.User;
+
+public class AuthorDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	
+	public AuthorDto() {
+	}
+	
+	public AuthorDto(User user) {
+		id = user.getId();
+		name = user.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
