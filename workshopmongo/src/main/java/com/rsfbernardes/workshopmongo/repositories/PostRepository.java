bb0747d56c8527336f02271dd6778519f9ef0f8d
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-09-23 10:33:33 -0300

    Consulta simples com query methods

--- workshopmongo/src/main/java/com/rsfbernardes/workshopmongo/repositories/PostRepository.java
+++ workshopmongo/src/main/java/com/rsfbernardes/workshopmongo/repositories/PostRepository.java
@@ -3,7 +3,11 @@ package com.rsfbernardes.workshopmongo.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.rsfbernardes.workshopmongo.domain.Post;
+import java.util.List;
+
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingAllIgnoringCase(String text);
 	
 }
