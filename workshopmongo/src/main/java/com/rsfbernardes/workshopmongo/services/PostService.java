ca299f1b2c34802ef0bac90776a29cd40a8b142d
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2023-09-23 08:54:49 -0300

    Obtendo um post por id

---
+++ workshopmongo/src/main/java/com/rsfbernardes/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.rsfbernardes.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rsfbernardes.workshopmongo.domain.Post;
+import com.rsfbernardes.workshopmongo.repositories.PostRepository;
+import com.rsfbernardes.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object Not Found!"));
+	}
+}
